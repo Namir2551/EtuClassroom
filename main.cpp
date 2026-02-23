bda52d93833411a857851a7c4d9a875097afdcd6
Namir2551 <namirkas22@gmail.com>
2026-02-23 11:02:40 -0500

    ajustement page main (menu)

--- main.cpp
+++ main.cpp
@@ -5,75 +5,94 @@
 #include "GestionnaireEtudiant.cpp"
 using namespace std;
 
- 
+int main()
+{
 
-int main() {
-     
     int option = 0;
-    
+
     GestionnaireEtudiant gestionnaire;
 
-    while (option != 4){ 
+    while (option != 4)
+    {
         cout << "===================== Menu principale =====================" << endl;
-        cout << "1. Creation d'une classe etudiant" << endl;
-        cout << "2. Creation Etudiant" << endl;
+        cout << "1. Gestion des classes" << endl;
+        cout << "2. Gestion des étudiants" << endl;
         cout << "3. Lister Etudiant" << endl;
         cout << "4. Exit" << endl;
         cout << "veilliez choisir une option: ";
         cin >> option;
         cout << endl;
-       
-        switch (option){
 
-            // creation d'une classe etudiant
-            case 1:{
-                cout << "========== Creation d'une classe etudiant ==========\n" << endl;
-                break;
-            }
-            // creation etudiant
-            case 2:{
-                printf("========== Creation Etudiant ========== \n");
-                // noubre de etudiant a cree 
-                int creenbrEtudiant; 
-                printf("veilliez saisir le nombre d'etudiant a cree: ");
-                cin >> creenbrEtudiant;
-                for(int i=0; i<creenbrEtudiant; i++){
-                    cout << "veilliez saisir le nom de l'etudiant " << i+1 << " : ";
-                    string nom;
-                    cin >> nom;
-                    cout << "veilliez saisir le prenom de l'etudiant " << i+1 << " : ";
-                    string prenom;
-                    cin >> prenom;
-                    Etudiant e(i+1, nom, prenom);
-                    gestionnaire.ajouterEtudiant(e);
+        switch (option)
+        {
 
-                    printf("\n");
-                }
-                break;
-            }
-            case 3:
-                printf("========== Lister Etudiant ========== \n");
-                // afficher la liste des etudiants
-                gestionnaire.afficherEtudiants();
-
-                break;
-            case 4:
-                cout << "Au revoir!" << endl;
-                exit(0);
-                break;
-
-            default:
-                cout << endl;
-                cout << "** Option invalide, veuillez reessayer **" << endl;
-                Sleep(2000); // pause de 2 secondes
-                break;
-
+        // creation d'une classe etudiant
+        case 1:
+        {
+            cout << "========== Creation d'une classe etudiant ==========\n"
+                 << endl;
+            break;
         }
-    }         
+        // creation etudiant
+        case 2:
+        {
+            printf("========== Gestion des etudiants ========== \n");
 
-    
+            int choix;
+            while (choix != 5)
+            {
+                printf("\nBienvenue au menu de la gestion des etudiants\n"
+                       "1. Création d'étudiant\n"
+                       "2. Supprimer un étudiant\n"
+                       "3. Modification\n"
+                       "4. Afficher la liste des étudiants\n"
+                       "5. Menu principale\n"
+                       "Veuillez sélectionner votre choix: ");
+
+                cin >> choix;
+
+                switch (choix)
+                {
+                case 1:
+                {
+                    gestionnaire.ajouterEtudiant();
+                    break;
+                }
+                case 2:
+                    // supprimer
+                    break;
+                case 3:
+                    gestionnaire.modifierEtudiant();
+                    break;
+                case 4:
+                    gestionnaire.afficherEtudiants();
+                    break;
+                case 5:
+                {
+                    break;
+                }
+                default:
+                    printf("** Choix invalide **");
+                }
+            }
+        }
+        case 3:
+        {
+
+            break;
+        }
+        case 4:
+            cout << "Au revoir!" << endl;
+            exit(0);
+            break;
+
+        default:
+            cout << endl;
+            cout << "** Option invalide, veuillez reessayer **" << endl;
+            Sleep(2000); // pause de 2 secondes
+            break;
+        }
+    }
 
     return 0;
 }
-
-
